936846f386f87642efdf2106385b3ca31faab55e
JoshBoshGames <161956647+JoshBoshGames@users.noreply.github.com>
2026-09-20 10:05:04 +0100

    Fix typo in request assignment

--- opencomputers/0826-Ind/discord_alerter_rs.lua
+++ opencomputers/0826-Ind/discord_alerter_rs.lua
@@ -40,7 +40,7 @@ while true do
     repeat
       computer.beep(20,timeout)
       timeot = timeout*2
-      local request, err = internet.request(constant.discord_url, payload, headers, "POST")
+      request, err = internet.request(constant.discord_url, payload, headers, "POST")
     until table.pack(request.response())[1] == 200
       computer.beep(200,0.1) computer.beep(250,0.1) computer.beep(300,0.1) computer.beep(400,0.1) -- Send Success SFX
   end
