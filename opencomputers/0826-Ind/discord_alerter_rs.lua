04996919c612434f28588635596c89ec83acca00
JoshBoshGames <161956647+JoshBoshGames@users.noreply.github.com>
2026-09-20 09:56:09 +0100

    Fix formatting in discord_alerter_rs.lua metadata

--- opencomputers/0826-Ind/discord_alerter_rs.lua
+++ opencomputers/0826-Ind/discord_alerter_rs.lua
@@ -3,7 +3,7 @@ meta={
   name="Discord Alerter Microcontroller - Redstone Trigger"
   author="JoshBoshGames",
   version="v0.0.1",
-  view_url="https://github.com/JoshBoshGames/minecraft-lua/blob/main/opencomputers/0826-Ind/discord_alerter_rs.lua"
+  view_url="https://github.com/JoshBoshGames/minecraft-lua/blob/main/opencomputers/0826-Ind/discord_alerter_rs.lua",
   raw_update_url="https://raw.githubusercontent.com/JoshBoshGames/minecraft-lua/refs/heads/main/opencomputers/0826-Ind/discord_alerter_rs.lua"
 }
 
