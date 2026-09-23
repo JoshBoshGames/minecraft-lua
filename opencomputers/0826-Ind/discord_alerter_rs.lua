605deec5ad5120272cb019d4c93d3b8fe8727244
JoshBoshGames <161956647+JoshBoshGames@users.noreply.github.com>
2026-09-23 20:43:31 +0100

    Remove response code check and sound effects
    Removed response code handling and associated sound effects.

--- opencomputers/0826-Ind/discord_alerter_rs.lua
+++ opencomputers/0826-Ind/discord_alerter_rs.lua
@@ -37,11 +37,5 @@ while true do
       ["Content-Type"] = "application/json"
     }
     request, err = internet.request(constant.discord_url, payload, headers, "POST")
-    response_code = tonumber(table.pack(request.response())[1])
-    if response_code >= 200 and response_code <=299 then
-      computer.beep(200,0.1) computer.beep(250,0.1) computer.beep(300,0.1) computer.beep(400,0.1) -- Send Success SFX
-    else
-      computer.beep(125,1) -- Send Fail SFX
-    end
   end
 end
